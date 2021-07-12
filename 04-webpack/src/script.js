951b35234cb4e8d74e3cfc8843e5d3199bf5c097
shubham2270 <shubham2270@gmail.com>
2021-07-12 20:33:44 +0530

    add webpack with threejs

---
+++ 04-webpack/src/script.js
@@ -0,0 +1,34 @@
+import "./style.css";
+import * as THREE from "three";
+
+console.log(THREE);
+
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1.5;
+camera.position.y = 1;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
