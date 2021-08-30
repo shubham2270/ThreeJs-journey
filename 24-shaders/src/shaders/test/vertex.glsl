e54f420acb354d455d189299f928cdb8a22955d2
shubham2270 <shubham2270@gmail.com>
2021-08-30 12:24:18 +0530

    shaders 1st lesson

--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -1,8 +1,11 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
-attribute vec3 position;
+
+varying vec2 vUv;
+varying float vElevation;
+
+// varying float vRandom;
 
 
 
@@ -10,10 +13,21 @@ void main()
  {  
    
    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-   modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+   modelPosition.z += elevation;
+  
+  // modelPosition.z += aRandom * 0.1;
 
    vec4 viewPosition = viewMatrix * modelPosition;
    vec4 projectedPosition = projectionMatrix * viewPosition;
 
    gl_Position = projectedPosition;
+
+  //  vRandom = aRandom;
+
+  vUv = uv;
+  vElevation = elevation;
  }
